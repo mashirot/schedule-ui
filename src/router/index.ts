dbadf6af4c9e562250b2a0097c608503a9c64477
mashirot <mashirotj@gmail.com>
2023-06-27 02:36:38 +0800

    路由模式更改

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
@@ -12,13 +12,13 @@ const router = createRouter({
           path: "",
           alias: ["l", "login"],
           name: "login",
-          component: () => import("@/views/login.vue")
+          component: () => import("@/views/Login.vue")
         },
         {
           path: "register",
           alias: "reg",
           name: "register",
-          component: () => import("@/views/register.vue")
+          component: () => import("@/views/Register.vue")
         },
       ]
     },
